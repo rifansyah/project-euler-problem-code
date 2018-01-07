22273a8de12662d8f23c23b97274193c6707a8c7
rifansyah <mokh.rifan.ardiansyah@gmail.com>
2018-01-08 04:32:43 +0700

    problem 14

---
+++ problem-14.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	list := make(map[int]int)
+	longestChain := 0
+	result := 0
+	for i := 1; i < 1000000; i++ {
+		temp := getSequence(list, i, 0)
+		if temp > longestChain {
+			result = i
+			longestChain = temp
+		}
+	}
+	fmt.Println(result, ":", longestChain, "chain")
+}
+
+func getSequence(list map[int]int, num, count int) int {
+	if val, ok := list[num]; ok {
+		return val + 1
+	}
+
+	if num == 1 {
+		return 1
+	}
+
+	if num%2 == 0 {
+		temp := num / 2
+		list[num] = getSequence(list, temp, 0) + 1
+	} else {
+		temp := 3*num + 1
+		list[num] = getSequence(list, temp, 0) + 1
+	}
+
+	return list[num]
+}
